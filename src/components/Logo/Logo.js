788c792b02c2132214b842b300ccf3f494f3c771
Mohsina-dot <smohsina108@gmail.com>
2025-12-02 03:10:00 +0530

    Final fix: Navbar import and file cleanup

--- src/components/Logo/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import logo from "../../assets/logo.png";
-
-function Logo() {
-  return <img src={logo} alt="QTify Logo" style={{ height: "40px" }} />;
-}
-
-export default Logo;
